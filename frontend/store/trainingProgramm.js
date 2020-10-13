d5a3877eb350f7c31655a270320dfd9a56394804
Safintim <timurtlt96@mail.ru>
2020-10-13 12:41:45 +0300

    Add prototype store for programms

---
+++ frontend/store/trainingProgramm.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    trainingPrograms: [],
+});
+
+export const mutations = {
+    setTrainingPrograms (state, programms) {
+        state.trainingPrograms = programms;
+    }
+};
+
+const trainingProgrammUrl = 'http://127.0.0.1:8000/api/training_programm/'
+
+export const actions = {
+    async fetch ({ commit }) {
+        const programms = await this.$axios.$get(trainingProgrammUrl);
+        commit('setTrainingPrograms', programms);
+    },
+};
+// TO DO INSTALL AXIOS
